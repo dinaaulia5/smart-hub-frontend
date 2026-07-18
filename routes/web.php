a74b291c25154d4f77151f0f5e4064f1a43e6389
dinaaulia5 <dinanr.aulia@gmail.com>
2026-07-18 10:24:03 +0700

    chore(routes): register room, equipment, booking and dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\EquipmentController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoomController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -34,5 +37,10 @@ Route::middleware('guest')->group(function () {
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->name('dashboard');
 
+//DATA MASTER 
 Route::resource('categories', CategoryController::class);
+Route::resource('rooms', RoomController::class);
+Route::resource('equipments', EquipmentController::class);
+
+Route::resource('bookings', BookingController::class);
 // require __DIR__.'/auth.php';
