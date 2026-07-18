1e937629642d6aa85a1b4a74f4d4bd1aa45172a8
dinaaulia5 <dinanr.aulia@gmail.com>
2026-07-18 11:30:28 +0700

    fix: routes web

--- routes/web.php
+++ routes/web.php
@@ -6,41 +6,25 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EquipmentController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\RoomController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 
 Route::middleware('guest')->group(function () {
-    Route::get('/', [LoginController::class, 'index'])->name('login');
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::get('/register', [RegisterController::class, 'index'])->name('register');
 });
+
+Route::redirect('/', '/dashboard');
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->name('dashboard');
 
-//DATA MASTER 
+// DATA MASTER
 Route::resource('categories', CategoryController::class);
 Route::resource('rooms', RoomController::class);
 Route::resource('equipments', EquipmentController::class);
-
 Route::resource('bookings', BookingController::class);
-// require __DIR__.'/auth.php';
